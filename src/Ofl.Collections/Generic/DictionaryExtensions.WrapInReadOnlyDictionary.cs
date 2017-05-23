50fcd5e99f823121da6bbbfc52ea7203aeed0db0
casperOne <casperOne@caspershouse.com>
2017-05-23 10:14:38 -0400

    Added WrapInReadOnlyDictionary to DictionaryExtensions.

---
+++ src/Ofl.Collections/Generic/DictionaryExtensions.WrapInReadOnlyDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Ofl.Collections.Generic
+{
+    public static partial class DictionaryExtensions
+    {
+        public static ReadOnlyDictionary<TKey, TValue> WrapInReadOnlyDictionary<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary)
+        {
+            // Validate parameters.
+            if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
+
+            // Wrap and return.
+            return new ReadOnlyDictionary<TKey, TValue>(dictionary);
+        }
+    }
+}
--- src/Ofl.Collections/Generic/HashHelpers.cs
+++ src/Ofl.Collections/Generic/HashHelpers.cs
@@ -6,8 +6,6 @@
         {
             unchecked
             {
-                // RyuJIT optimizes this to use the ROL instruction
-                // Related GitHub pull request: dotnet/coreclr#1830
                 uint rol5 = ((uint)h1 << 5) | ((uint)h1 >> 27);
                 return ((int)rol5 + h1) ^ h2;
             }
