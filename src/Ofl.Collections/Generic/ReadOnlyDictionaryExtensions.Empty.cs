50c83834a577ca75199da3276a0232a03ff81786
casperOne <casperOne@caspershouse.com>
2018-12-30 16:33:12 -0500

    Added ReadOnlyDictionaryExtensions.Empty.

---
+++ src/Ofl.Collections/Generic/ReadOnlyDictionaryExtensions.Empty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Ofl.Collections.Generic
+{
+    public static partial class ReadOnlyDictionaryExtensions
+    {
+        public static ReadOnlyDictionary<TKey, TValue> Empty<TKey, TValue>() =>
+            EmptyReadOnlyDictionarySingleton<TKey, TValue>.Instance;
+
+        private static class EmptyReadOnlyDictionarySingleton<TKey, TValue>
+        {
+            internal static readonly ReadOnlyDictionary<TKey, TValue> Instance = 
+                new ReadOnlyDictionary<TKey, TValue>(new Dictionary<TKey, TValue>());
+        }
+    }
+}
