f03272a8e2c677a3927776a295ecd11f44e58f7c
casperOne <casperOne@caspershouse.com>
2020-02-23 01:59:48 -0500

    Added TryGetValue.

---
+++ src/Ofl.Collections/Generic/DictionaryExtensions.TryGetValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ofl.Collections.Generic
+{
+    public static partial class DictionaryExtensions
+    {
+        public static TValue? TryGetValue<TKey, TValue>(
+            this Dictionary<TKey, TValue> dictionary,
+            TKey key            
+        )
+            where TKey : notnull
+            where TValue : struct
+        {
+            // Validate parameters.
+            if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            // Get the value, if it fails, return default.
+            return dictionary.TryGetValue(key, out TValue value) ? value : (TValue?) null;
+        }
+    }
+}
---
+++ src/Ofl.Collections/Generic/ReadOnlyDictionaryExtensions.TryGetValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Ofl.Collections.Generic
+{
+    public static partial class ReadOnlyDictionaryExtensions
+    {
+        public static TValue? TryGetValue<TKey, TValue>(
+            this ReadOnlyDictionary<TKey, TValue> dictionary,
+            TKey key            
+        )
+            where TKey : notnull
+            where TValue : struct
+        {
+            // Validate parameters.
+            if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            // Get the value, if it fails, return default.
+            return dictionary.TryGetValue(key, out TValue value) ? value : (TValue?) null;
+        }
+    }
+}
